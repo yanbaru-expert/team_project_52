c2a8c4c9eeeba2ac5baf16c091fdd4e78ba91c79
YOU310 <75014820+YOU310@users.noreply.github.com>
2021-05-21 11:52:13 +0900

    rubocop -aを実行

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-email = 'test@example.com'
-password = 'password'
+email = "test@example.com"
+password = "password"
 
 # テストユーザーが存在しないときだけ作成
 User.find_or_create_by!(email: email) do |user|
   user.password = password
-  puts 'ユーザーの初期データインポートに成功しました。'
+  puts "ユーザーの初期データインポートに成功しました。"
 end
