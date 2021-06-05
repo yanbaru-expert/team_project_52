1f2a422e17e697e6badc1edb8089f8ad1f11375f
大津　秀介 <otsu0412@yahoo.co.jp>
2021-06-05 12:50:16 +0900

    ボタン追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap/dist/js/bootstrap"
+
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import "bootstrap/dist/js/bootstrap"
+
