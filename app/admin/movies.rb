0522732cda353d1519b344d929e8f3d9f2bacc15
clocknote2222 <clocknote2022@gmail.com>
2021-06-07 14:00:25 +0900

    app/admin/movies.rb, texts.rbの修正

--- app/admin/movies.rb
+++ app/admin/movies.rb
@@ -9,4 +9,31 @@ ActiveAdmin.register Movie do
     column :updated_at
     actions
   end
+
+  show do
+    attributes_table do
+      row :id
+      row :genre, :movie, &:genre_i18n
+      row :url
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      input :genre, as: :select, collection: Movie.genres_i18n.invert
+      input :title
+      input :url
+    end
+    f.actions
+  end
+
+  filter :genre, as: :select, collection: Movie.genres_i18n.invert.transform_values { |v| Movie.genres[v] }
+  filter :title
+  filter :url
+  filter :created_at
+  filter :updated_at
 end
--- app/admin/texts.rb
+++ app/admin/texts.rb
@@ -9,4 +9,31 @@ ActiveAdmin.register Text do
     column :updated_at
     actions
   end
+
+  show do
+    attributes_table do
+      row :id
+      row :genre, :text, &:genre_i18n
+      row :content
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      input :genre, as: :select, collection: Text.genres_i18n.invert
+      input :title
+      input :content
+    end
+    f.actions
+  end
+
+  filter :genre, as: :select, collection: Text.genres_i18n.invert.transform_values { |v| Text.genres[v] }
+  filter :title
+  filter :content
+  filter :created_at
+  filter :updated_at
 end
