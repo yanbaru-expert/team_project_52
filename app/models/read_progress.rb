931e71073c8e642dae65fed5fa09c9c563ea8b79
Ubuntu <ubuntu@ip-172-31-40-118.ap-northeast-1.compute.internal>
2021-06-12 08:33:58 +0000

    read_progress.rbとja.ymlの編集

--- app/models/read_progress.rb
+++ app/models/read_progress.rb
@@ -1,4 +1,9 @@
 class ReadProgress < ApplicationRecord
   belongs_to :user
   belongs_to :text
+
+  validates :user_id, uniqueness: {
+    scope: :text_id,
+    message: "は同じ投稿に2回以上いいねはできません"
+  }
 end
