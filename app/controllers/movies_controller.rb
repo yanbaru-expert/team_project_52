16e7690a4de617067825b39724db8b20eb906df8
clocknote2222 <clocknote2022@gmail.com>
2021-06-13 09:12:46 +0900

    ２ページ目以降のレベル表示を連番出力

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,5 +3,7 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.where(genre: Movie::RAILS_GENRE_LIST).page(params[:page]).per(PER_PAGE)
+    page = @movies.current_page
+    @movie_counter = (page - 1) * PER_PAGE
   end
 end
