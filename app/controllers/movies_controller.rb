0a6f074152eb09c4b51cc627f1aa5b8b8f922907
YOU310 <75014820+YOU310@users.noreply.github.com>
2021-06-14 22:37:42 +0900

    def indexを書き換え

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,12 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.where(genre: Movie::RAILS_GENRE_LIST).includes(:watch_progresses)
+    if params[:genre] == "php"
+      @movies = Movie.where(genre: :php).includes(:watch_progresses)
+      @title = "PHP動画"
+    else
+      @movies = Movie.where(genre: Movie::RAILS_GENRE_LIST).includes(:watch_progresses)
+      @title = "Ruby/Rails 動画"
+    end
+    # @movies = Movie.where(genre: Movie::RAILS_GENRE_LIST).includes(:watch_progresses)
   end
 end
