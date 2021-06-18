eff968c30c21dce039eef26f00b889751c50e461
clocknote2222 <clocknote2022@gmail.com>
2021-06-18 09:05:14 +0900

    page_title(genre)の修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,10 +11,6 @@ module ApplicationHelper
   end
 
   def page_title(genre)
-    genre == "php" ? "PHP動画" : "Ruby/Rails 動画"
-  end
-
-  def page_text(genre)
-    genre == "php" ? "PHPテキスト教材ページ" : "Ruby/Rails テキスト教材"
+    genre == "php" ? "PHP" : "Ruby/Rails"
   end
 end
