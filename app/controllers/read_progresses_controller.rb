d4187772d007bb15fd31bc4fe8d7155a8ff30ab9
Ubuntu <ubuntu@ip-172-31-40-118.ap-northeast-1.compute.internal>
2021-06-20 08:38:50 +0000

    redirect_to root_pathをコントローラーから消去

--- app/controllers/read_progresses_controller.rb
+++ app/controllers/read_progresses_controller.rb
@@ -2,12 +2,10 @@ class ReadProgressesController < ApplicationController
   def create
     @text = Text.find(params[:text_id])
     current_user.read_progresses.create!(text_id: @text.id)
-    redirect_to root_path
   end
 
   def destroy
     @text = Text.find(params[:text_id])
     current_user.read_progresses.find_by(text_id: @text.id).destroy!
-    redirect_to root_path
   end
 end
