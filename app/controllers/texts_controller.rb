4a1104f1fd7f84d78e640f7d1188068537f07acd
Ubuntu <ubuntu@ip-172-31-40-118.ap-northeast-1.compute.internal>
2021-06-20 10:29:58 +0000

    非同期の編集

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,6 +1,6 @@
 class TextsController < ApplicationController
   def index
-    @texts = Text.where(genre: Text::RAILS_GENRE_LIST).order(id: :asc)
+    @texts = Text.where(genre: Text::RAILS_GENRE_LIST).includes(:read_progresses).order(id: :asc)
   end
 
   def show; end
